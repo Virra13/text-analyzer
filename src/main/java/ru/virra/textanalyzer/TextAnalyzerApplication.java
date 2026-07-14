4335af5b435bb47377bbe02f519c77a3f2d4bbdf
Virra13 <sha.elvira@mail.ru>
2026-07-14 12:59:46 +0300

    nitial pringoot project

---
+++ src/main/java/ru/virra/textanalyzer/TextAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ru.virra.textanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextAnalyzerApplication.class, args);
+    }
+
+}
