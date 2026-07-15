3addd851fbf674791e7a66754ab3eca404cc6336
Virra13 <sha.elvira@mail.ru>
2026-07-15 08:18:09 +0300

    Добавлен вывод help-справки

--- src/main/java/ru/virra/textanalyzer/application/ConsoleRunner.java
+++ src/main/java/ru/virra/textanalyzer/application/ConsoleRunner.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import ru.virra.textanalyzer.InvalidArgumentsException;
 import ru.virra.textanalyzer.cli.AnalysisConfig;
 import ru.virra.textanalyzer.cli.CliService;
 import ru.virra.textanalyzer.cli.HelpPrinter;
@@ -17,11 +18,20 @@ public class ConsoleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
         if (args.containsOption("help")) {
             helpPrinter.print();
+            return;
+        }
 
-        } else {
+        try {
             AnalysisConfig config = cliService.parseArgs(args);
+
+
+
+        } catch (InvalidArgumentsException e) {
+            System.err.println(e.getMessage());
+            System.err.println("Use --help to see available options.");
         }
     }
 }
--- src/main/java/ru/virra/textanalyzer/cli/HelpPrinter.java
+++ src/main/java/ru/virra/textanalyzer/cli/HelpPrinter.java
@@ -2,11 +2,31 @@ package ru.virra.textanalyzer.cli;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 @Component
 public class HelpPrinter {
 
     public void print() {
-        System.out.println("help");
-    }
 
+        try (InputStream input = HelpPrinter.class.getClassLoader().getResourceAsStream("help.txt")) {
+            if (input == null) {
+                System.err.println("Help file not found.");
+                return;
+            }
+            String text = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+
+            System.out.println(text);
+        }
+        catch (IOException e) {
+            System.err.println("Failed to read help file: " + e.getMessage());
+        }
+    }
 }
